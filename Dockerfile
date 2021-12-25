cff767f48cc06b9c608861a4f6a6488aa9a51194
sinhvienit17 <nguyentuanquangsang1999@gmail.com>
2021-12-26 04:24:45 +0700

    change

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:14.18
-WORKDIR /front-end-reactjs
-RUN apk --update add git
-COPY package.json .
-RUN yarn
-COPY . .
-# RUN yarn build
-CMD ["yarn", "start"]
-
--- src/apolo-client/config.js
+++ src/apolo-client/config.js
@@ -1,7 +1,7 @@
 export const ENVIRONMENT = 'dev'
 //export const ENVIRONMENT = "production";
-const HOST = '192.168.98.74'
-const PORT = '3001'
+const HOST = '54.159.116.102'
+const PORT = process.env.PORT || '3001'
 
 const apiEnv = {
   dev: `http://${HOST}:${PORT}/admin/api`,
