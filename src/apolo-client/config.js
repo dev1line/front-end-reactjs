4a54afd9308f83f86d058c885b0e6642fa2aac7e
sinhvienit17 <nguyentuanquangsang1999@gmail.com>
2021-12-30 01:33:48 +0700

    fe

--- src/apolo-client/config.js
+++ src/apolo-client/config.js
@@ -1,15 +1,10 @@
 export const ENVIRONMENT = 'dev'
 // export const ENVIRONMENT = 'production'
-const HOST_PD = 'cms-keystonejs.herokuapp.com'
-const HOST = 'cms-keystonejs.herokuapp.com'
-//'54.159.116.102'
-const PORT = '3003' //process.env.PORT ||
+const HOST = 'cms-keystonejs.herokuapp.com' //'54.159.116.102'
+const PORT = process.env.PORT || '3001'
 
 const apiEnv = {
-  // dev:
-  dev: `http://${HOST}:${PORT}/admin/api`,
-  production: `https://${HOST_PD}/admin/api`,
+  dev: `https://${HOST}/admin/api`,
 }
 
 export const API_CMS = apiEnv['dev']
-export const SERVER_API = `http://${HOST}/admin/api`
