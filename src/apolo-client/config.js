289b9835e5edeaa11102569259001ebcfda74534
sinhvienit17 <nguyentuanquangsang1999@gmail.com>
2022-01-24 23:20:25 +0700

    ps

--- src/apolo-client/config.js
+++ src/apolo-client/config.js
@@ -1,10 +1,11 @@
 export const ENVIRONMENT = 'dev'
-// export const ENVIRONMENT = 'production'
+export const ENVIRONMENT = 'production'
 const HOST = 'localhost' //'cms-keystonejs.herokuapp.com' //'54.159.116.102'
 const PORT = process.env.PORT || '3001'
 
 const apiEnv = {
   dev: `http://${HOST}:${PORT}/admin/api`,
+  production: 'https://cms-keystonejs.herokuapp.com'
 }
 
 export const API_CMS = apiEnv['dev']
